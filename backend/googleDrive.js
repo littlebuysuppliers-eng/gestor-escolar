95e54b491036848fc0dd40bae762a1e0a6735895
littlebuysuppliers-eng <littlebuysuppliers@gmail.com>
2025-10-08 18:17:05 -0600

    Update googleDrive.js

--- backend/googleDrive.js
+++ backend/googleDrive.js
@@ -1,51 +1,38 @@
 import { google } from 'googleapis';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
-
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
 const auth = new google.auth.GoogleAuth({
-  credentials,
-  scopes: ['https://www.googleapis.com/auth/drive.file'],
+  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
+  scopes: SCOPES
 });
 
 const drive = google.drive({ version: 'v3', auth });
-const MAIN_FOLDER_ID = '13J6veloK9YmCaCFACxCXrrK6ZbDemZdA';
 
-// Crear subcarpeta por usuario y subir archivo
-export async function uploadToDrive(userId, file) {
-  const folderName = `usuario_${userId}`;
-  const folderId = await ensureUserFolder(folderName);
-
-  const response = await drive.files.create({
-    requestBody: {
-      name: file.originalname,
-      parents: [folderId],
-    },
-    media: {
-      mimeType: file.mimetype,
-      body: file.buffer,
-    },
-    fields: 'id, name, webViewLink',
-  });
-
-  return response.data;
+// Crear carpeta
+export async function createFolder(name, parentId) {
+  const fileMetadata = {
+    name,
+    mimeType: 'application/vnd.google-apps.folder',
+    parents: parentId ? [parentId] : []
+  };
+  const res = await drive.files.create({ resource: fileMetadata, fields: 'id' });
+  return res.data.id;
 }
 
-async function ensureUserFolder(folderName) {
-  const res = await drive.files.list({
-    q: `'${MAIN_FOLDER_ID}' in parents and name='${folderName}' and mimeType='application/vnd.google-apps.folder' and trashed=false`,
-    fields: 'files(id, name)',
-  });
-
-  if (res.data.files.length > 0) return res.data.files[0].id;
-
-  const folder = await drive.files.create({
+// Subir archivo
+export async function uploadFile(fileName, folderId, fileBuffer) {
+  const res = await drive.files.create({
     requestBody: {
-      name: folderName,
-      mimeType: 'application/vnd.google-apps.folder',
-      parents: [MAIN_FOLDER_ID],
+      name: fileName,
+      parents: [folderId]
     },
-    fields: 'id',
+    media: {
+      mimeType: 'application/octet-stream',
+      body: Buffer.from(fileBuffer)
+    },
+    fields: 'id'
   });
-
-  return folder.data.id;
+  return res.data.id;
 }
